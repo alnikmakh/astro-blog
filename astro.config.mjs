9990eef4fd2f21813d3c75a77a3eb8da7605cc0c
Aleksandr Makhorin <alnikmakh@gmail.com>
2025-03-30 18:34:08 +0200

    fix shoelace import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,23 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import copy from "rollup-plugin-copy";
 
 // https://astro.build/config
-export default defineConfig({});
-
+export default defineConfig({
+  vite: {
+    plugins: [
+      {
+        ...copy({
+          copyOnce: true,
+          hook: "buildStart",
+          targets: [
+            {
+              src: "node_modules/@shoelace-style/shoelace/dist/assets",
+              dest: "public/shoelace-assets",
+            },
+          ],
+        }),
+        apply: "build",
+      },
+    ],
+  },
+});
